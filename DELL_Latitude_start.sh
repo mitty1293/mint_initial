f91699f5d4e69c8cab11cf11c44da42f5201ab44
mitty1293 <46413939+mitty1293@users.noreply.github.com>
2021-02-03 22:45:11 +0900

    Update DELL_Latitude_start.sh

--- DELL_Latitude_start.sh
+++ DELL_Latitude_start.sh
@@ -45,6 +45,10 @@ add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(
 apt-get update -y
 apt-get install -y docker-ce
 
+echo "install docker-compose"
+curl -L https://github.com/docker/compose/releases/download/1.26.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
 echo "install vscode"
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
