538c2100ab5c630d227457c6867ee9218ac55acb
mitty1293 <46413939+mitty1293@users.noreply.github.com>
2021-02-03 21:30:25 +0900

    Update DELL_Latitude_start.sh

--- DELL_Latitude_start.sh
+++ DELL_Latitude_start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo upgrade_Log_start
+echo "upgrade start"
 apt update -y
 apt upgrade -y
 
-echo vim_Log_start
+echo "install vim"
 apt install -y vim
 
-echo ssh_Log_start
+echo "install ssh"
 ssh_var1="#PermitRootLogin .*"
 ssh_var2="PermitRootLogin no"
 apt install -y openssh-server
@@ -16,14 +16,14 @@ sed -i -e "s/$ssh_var1/$ssh_var2/g" /etc/ssh/sshd_config
 systemctl enable ssh
 systemctl restart ssh
 
-echo HandleLidSwitch_Log_start
+echo "set HandleLidSwitch"
 hand_var1="#HandleLidSwitch=suspend"
 hand_var2="HandleLidSwitch=ignore"
 cp /etc/systemd/logind.conf /etc/systemd/logind.conf.orig
 sed -i -e "s/$hand_var1/$hand_var2/g" /etc/systemd/logind.conf
 systemctl restart systemd-logind
 
-echo RDP_Log_start
+echo "install RDP"
 rdp_var1="unset DBUS_SESSION_BUS_ADDRESS"
 rdp_var2="exec mate-session"
 apt install -y xserver-xorg-core
@@ -34,11 +34,11 @@ sed -i -e "32i $rdp_var1" /etc/xrdp/startwm.sh
 sed -i -e "33i $rdp_var2" /etc/xrdp/startwm.sh
 systemctl restart xrdp
 
-echo VirtulBox_Log_start
+echo "install VirtulBox"
 apt install -y openjdk-11-jdk
 apt install -y virtualbox virtualbox-guest-additions-iso virtualbox-dkms virtualbox-qt
 
-echo Docker_Log_start
+echo "install Docker"
 apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(. /etc/os-release; echo "$UBUNTU_CODENAME") stable"
